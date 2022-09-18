46ab8e91423b23b2f4bbb8a68dc7f389a62be51d
WillieJameson <williejamesons26@gmail.com>
2022-09-18 23:57:16 +0700

    Stilll solviing particle js

--- tedx-website/src/sections/HeroSection.js
+++ tedx-website/src/sections/HeroSection.js
@@ -23,90 +23,123 @@ export default function HeroSection() {
             <img src={Logo2} class="image one-logo" alt="logo" />
           </div>
         </div>
+        
       </div>
-      <Particles
+        <Particles
         className="particle"
         height="700px"
         id="tsparticles"
         options={{
-          background: {
-            color: {
-              value: "#000000",
+          "particles": {
+            "number": {
+              "value": 80,
+              "density": {
+                "enable": true,
+                "value_area": 800
+              }
             },
+            "color": {
+              "value": "#ffffff"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 5
+              },
+              "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+              }
+            },
+            "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 3,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": true,
+              "distance": 150,
+              "color": "#ffffff",
+              "opacity": 0.5,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 6,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "bounce",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+              }
+            }
           },
-          fpsLimit: 60,
-          interactivity: {
-            detectsOn: "canvas",
-            events: {
-              onClick: {
-                enable: false,
-                mode: "push",
+          "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": {
+                "enable": false,
+                "mode": "repulse"
               },
-              onHover: {
-                enable: false,
-                mode: "repulse",
+              "onclick": {
+                "enable": false,
+                "mode": "push"
               },
-              resize: false,
+              "resize": true
             },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
+            "modes": {
+              "grab": {
+                "distance": 419.58041958041963,
+                "line_linked": {
+                  "opacity": 0.5584919099613911
+                }
               },
-              push: {
-                quantity: 4,
+              "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
               },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
+              "repulse": {
+                "distance": 200,
+                "duration": 0.4
               },
-            },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
           },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              random: true,
-              value: 3,
-            },
-          },
-          detectRetina: true,
+          "retina_detect": true
         }}
-      />
+        />
     </div>
   );
 }
